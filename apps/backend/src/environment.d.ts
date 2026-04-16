001eb39cd946e9b30c7019fe74508dab7840e05e
Anton <anton.kim.wns@gmail.com>
2026-04-16 18:37:14 +0300

    [FIX] Разные фиксы для сборки образа

---
+++ apps/backend/src/environment.d.ts
@@ -0,0 +1,33 @@
+export {};
+
+import { UserInfo } from "@custom_types/userinfo";
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production'; 
+            JWT_VER: `${number}`;
+            JWT_EXP: `${number}`;
+            JWT_REFRESH_EXP: `${number}`;
+            JWT_SECRET: string;
+            JWT_REFRESH_SALT: string;
+            DB_USER: string;
+            DB_PASS: string;
+            DB_HOST: string;
+            DB_PORT: string;
+            AUTH_SERVICE_URL: string;
+            AUTH_SERVICE_DB: string;
+            USERS_SERVICE_URL: string;
+            USERS_SERVICE_DB: string;
+            APPOINTMENTS_SERVICE_URL: string;
+            APPOINTMENTS_SERVICE_DB: string;
+        }
+    }
+
+    namespace Express {
+        interface Request {
+            user?: UserInfo;
+            parsedQuery?: Record<string, string>;
+        }
+    }
+}
